5f875d777cfba10c018da5cc55ab3c2f2ab54c30
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-10 19:26:22 +0200

    Second Commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from .views import list_books
+from .views import LibraryDetailView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Book/', Views.books, name = 'books')
-    path('Library', Views.Library_books.as_Library(), name = 'Library')
+    path('Book/', Views.list_books, name = 'books')
+    path('Library', Views.LibraryDetailView.as_Library(), name = 'Library')
 ]
