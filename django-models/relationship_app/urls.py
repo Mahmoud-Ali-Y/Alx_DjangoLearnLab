c0189f152150df1bfc59d0a9efa26e6ade0eee13
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-10 23:08:22 +0200

    Fourth

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login')
     path('logout/', LogoutView.as_view(), name='logout')
     path('views.register/', LogoutView.as_view(template_name='relationship_app/register.html'))
+    path('add_book/')
+    path('edit_book/')
+    path('delete_book/')
 ]
