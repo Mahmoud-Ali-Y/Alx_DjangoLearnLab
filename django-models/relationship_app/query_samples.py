f4f6a11fa08648443514b88d9b3283d1feef1e13
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-10 08:57:56 +0200

    First Commit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,13 @@
 from .models import *
 # Query all books by a specific author.
-author_name = Author.objects.filter(name = '')
+author_name = pass
 author_books = Book.objects.filter(author = author_name)
 
 # List all books in a library.
-library_books = Library.objects.filter(name = '')
-library_books_details = Book.objects.filter(book = library_books)
-
+# library_books = Library.objects.filter(name = '')
+# library_books_details = Book.objects.filter(book = library_books)
+library_name = pass
+books = Library.objects.get(name=library_name)
+books.all()
 # Retrieve the librarian for a library.
 library_librarian = Librarian.objects.filter(Library = '')
