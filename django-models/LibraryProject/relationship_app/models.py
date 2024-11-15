ec87e1f2e383ac3341badf36e61fdeb9c4864945
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-15 15:11:06 +0200

    Fifth

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=200)
+    def __unicode__(self): 
+      return self.name
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+    class Meta(type):
+        permissions = ("can_add_book", "can_change_book", "can_delete_book")
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book, related_name='books')
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role_choices = ("Admin", "Librarian", "Member")
+    role = models.CharField(max_length=1, choices=role_choices)
