68ae5b6bdff0f7b5c171cde553bc9cf89d5f81f4
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-17 11:37:31 +0200

    First

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -12,3 +12,10 @@ class CustomUser(AbstractUser):
         user = CustomUser.objects.create_user(self.date_of_birth, self.profile_photo)
     def create_superuser(self):
         user = CustomUser.objects.create_superuser(self.date_of_birth, self.profile_photo)
+class CustomUserManager(BaseUserManager):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+    def create_user(date_of_birth, profile_photo):
+        user = CustomUser.objects.create_user(date_of_birth, profile_photo)
+    def create_superuser(date_of_birth, profile_photo):
+        user = CustomUser.objects.create_superuser(date_of_birth, profile_photo)
