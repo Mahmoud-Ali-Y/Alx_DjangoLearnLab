ff20e69eb7ab5690153ee30546d2827146280a40
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-17 13:07:44 +0200

    Second

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from .models import *
 from django.contrib.auth.models import Permission
 from django.contrib.auth.decorators import permission_required
+from django.template import loader
 # Create your views here.
 @permission_required('app_name.can_edit', raise_exception=True)
-def create_book(request):
-    new_book = Book.objects.create()
+def list_books(request):
+  books = Book.objects.all().values()
+  template = loader.get_template('relationship_app/list_books.html')
+  context = {
+    'books': books,
+  }
+@permission_required('app_name.can_edit', raise_exception=True)
+def books(request):
+  books = Book.objects.all().values()
+  template = loader.get_template('relationship_app/list_books.html')
+  context = {
+    'books': books,
+  }
