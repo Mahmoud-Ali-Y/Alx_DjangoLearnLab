3e41dac13e6e74a56230cbf1f0054c7c2a1e5941
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-17 11:33:18 +0200

    First

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Book
-# Register your models here.
-
-admin.site.register(Book)
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Book, CustomUser
 class Book(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('author', 'publication_year')
+
+class CustomUserInline(admin.StackedInline):
+    model = CustomUser
+    can_delete = False
+    verbose_name_plural = "CustomUser"
+class UserAdmin(BaseUserAdmin):
+    inlines = [CustomUserInline]
+
+# Register your models here.
+admin.site.register(Book)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 # Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+    def create_user(self):
+        user = CustomUser.objects.create_user(self.date_of_birth, self.profile_photo)
+    def create_superuser(self):
+        user = CustomUser.objects.create_superuser(self.date_of_birth, self.profile_photo)
