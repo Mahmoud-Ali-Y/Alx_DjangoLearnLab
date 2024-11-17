8db2113267087b9ae6bc3486d66c352606ebdc1c
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-17 13:04:26 +0200

    Second

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser, BaseUserManager
+from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    can_view_permission = Permission.objects.create(
+    codename="can_view",
+    name="Can view books",
+    )
+    can_create_permission = Permission.objects.create(
+    codename="can_create",
+    name="Can create books",
+    )
+    can_edit_permission = Permission.objects.create(
+    codename="can_edit",
+    name="Can edit books",
+    )
+    can_delete_permission = Permission.objects.create(
+    codename="can_delete",
+    name="Can delete books",
+    )
 # Create your models here.
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
@@ -19,3 +37,8 @@ class CustomUserManager(BaseUserManager):
         user = CustomUser.objects.create_user(date_of_birth, profile_photo)
     def create_superuser(date_of_birth, profile_photo):
         user = CustomUser.objects.create_superuser(date_of_birth, profile_photo)
+class Editors(Group):
+    description = models.TextField(blank=True)
+ #   group = Group.objects.create(name='Editors')
+    group = Group.objects.get(name='Editors')
+    group.permissions.add(Book.can_view_permission, Book.can_create_permission, Book.can_edit_permission)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib.auth.models import Permission
+from django.contrib.auth.decorators import permission_required
 # Create your views here.
+@permission_required('app_name.can_edit', raise_exception=True)
+def create_book(request):
+    new_book = Book.objects.create()
