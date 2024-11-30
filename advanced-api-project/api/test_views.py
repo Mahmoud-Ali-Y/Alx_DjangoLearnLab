3f589452775af4360ebd4f7c5aa7dece878b6f27
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-30 21:03:15 +0200

    Fourth

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,14 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase 
 from rest_framework.test import APIRequestFactory
 from .models import *
 
-def setUp(self):
-    Author.objects.create(name="lion")
-    Book.objects.create(title = 'Forest', publication_year = 2000, author = 'lion' )
+class APITestCase:
+ def setUp(self):
+     Author.objects.create(name="lion")
+     Book.objects.create(title = 'Forest', publication_year = 2000, author = 'lion' )
 
-def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        book = Book.objects.get(title="Forest")
-        self.assertEqual(Book.view())
-        self.assertEqual(Book.update())
-        self.assertEqual(Book.delete())
+ def test_books_can_updated(self):
+         """Animals that can speak are correctly identified"""
+         book = Book.objects.get(title="Forest")
+         self.assertEqual(Book.view())
+         self.assertEqual(Book.update())
+         self.assertEqual(Book.delete())
