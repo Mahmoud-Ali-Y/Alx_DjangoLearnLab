aed9ed811d080f971e13b9b2f940535d51b47f28
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-11-30 20:46:41 +0200

    Third

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,8 @@ from .serializers import BookSerializer # replace with your project's serializer
 from .models import Book
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
+from rest_framework import filters
 
 # Create your views here.
 class CustomBookCreateView(generics.CreateAPIView):
@@ -21,6 +23,18 @@ class CustomBookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'publication_year', 'author']
+    ordering_fields = ['title', 'publication_year', 'author']
+
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        title = self.request.title
+        return Book.objects.filter(title=title)
 
 class CustomBookDetailView(generics.ListAPIView):
     authentication_classes = [TokenAuthentication]
