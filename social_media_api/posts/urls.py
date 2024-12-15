c888f0b3018dd94ae236c0191aeeaa43571fa2d9
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-12-15 23:18:20 +0200

    Third

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from posts.models import *
-from posts.views import PostViewSet, CommentViewSet
+from posts.views import PostViewSet, CommentViewSet, FeedView
 from django.contrib.auth.views import LogoutView
 """
 """
@@ -13,5 +13,6 @@ router.register(r'Comment', CommentViewSet)
 
 urlpatterns = [
     path('posts/', include(router.urls)),
+    path('feed/', FeedView.as_view(),name='login'),
     ]
 #"""
